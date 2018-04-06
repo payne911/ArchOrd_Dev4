e8421a95783bd5160414af88f5d1a52248a90340
payne911 <payne911@users.noreply.github.com>
2018-04-05 20:22:30 -0400

    first_test
    Bob

---
+++ src/zeroext.vhd
@@ -0,0 +1,11 @@
+library IEEE; use IEEE.STD_LOGIC_1164.all;
+
+entity zeroext is -- zero extender
+	port (a: in STD_LOGIC_VECTOR (15 downto 0);
+			y: out STD_LOGIC_VECTOR (31 downto 0));
+end;
+
+architecture behave of zeroext is
+begin
+		y <= X"0000" & a;
+end;
