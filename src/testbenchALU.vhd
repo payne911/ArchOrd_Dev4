807816d39d2a5a3568b040ee3328bfcfe668d05d
payne911 <payne911@users.noreply.github.com>
2018-04-27 17:08:27 -0400

    lil' mods! :)

--- src/testbenchALU.vhd
+++ src/testbenchALU.vhd
@@ -17,7 +17,7 @@ architecture test of testbench is
     signal alucontrol: STD_LOGIC_VECTOR (5 downto 0);
 	signal shamt: STD_LOGIC_VECTOR(4 downto 0);
     signal zero, overflow : STD_LOGIC;
-    signal aluOut: STD_LOGIC_VECTOR(31 downto 0));
+    signal aluOut: STD_LOGIC_VECTOR(31 downto 0);
 
 begin
 -- instantiate device to be tested
@@ -31,11 +31,11 @@ process begin
     -- sll
 	alucontrol <= "000000";
 	wait for 5 ns;
-	if (aluout = X"00000010" and z = '0') then
+	if (aluout = X"00000010") then
 		report "sll : OK";
 	else
 		report "sll : NOTOK - wrong value";
-	endif;
+	end if;
 
 	srca <= X"80000000";
 	srcb <= X"7FFFFFFF";
@@ -45,76 +45,77 @@ process begin
 	wait for 5 ns;
 	if (overflow = '1') then
 		report "add : NOTOK - overflow";
-	else if (aluout = X"FFFFFFFF" and z = '0') then
+	else if (aluout = X"FFFFFFFF") then
 		report "add : OK";
 	else
 		report "add : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- sub
 	alucontrol <= "100010";
 	wait for 5 ns;
 	if (overflow = '1') then
 		report "sub : NOTOK - overflow";
-	else if (aluout = X"00000001" and z = '0') then
+	else if (aluout = X"00000001") then
 		report "sub : OK";
 	else
 		report "sub : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- and
 	alucontrol <= "100100";
 	wait for 5 ns;
-	if (aluout = X"00000000" and z = '1') then
+	if (aluout = X"00000000") then
 		report "and : OK";
 	else
 		report "and : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- or
 	alucontrol <= "100101";
 	wait for 5 ns;
-	if (aluout = X"FFFFFFFF" and z = '0') then
+	if (aluout = X"FFFFFFFF") then
 		report "or : OK";
 	else
 		report "or : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- xor
 	alucontrol <= "100110";
 	wait for 5 ns;
-	if (aluout = X"FFFFFFFF" and z = '0') then
+	if (aluout = X"FFFFFFFF") then
 		report "xor : OK";
 	else
 		report "xor : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- nor
 	alucontrol <= "100111";
 	wait for 5 ns;
-	if (aluout = X"00000000" and z = '1') then
+	if (aluout = X"00000000") then
 		report "nor : OK";
 	else
 		report "nor : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- sltu
 	alucontrol <= "101011";
 	wait for 5 ns;
-	if (aluout = X"00000000" and z = '1') then
+	if (aluout = X"00000000") then
 		report "sltu : OK";
 	else
 		report "sltu : NOTOK - wrong value";
-	endif;
+	end if;
 
     -- slt
 	alucontrol <= "101010";
 	wait for 5 ns;
-	if (aluout = X"00000001" and z = '0') then
+	if (aluout = X"00000001") then
 		report "slt : OK";
 	else
 		report "slt : NOTOK - wrong value";
-	endif;
-    wait; -- forever
+	end if;
+	
+wait; -- forever
 end process;
 end;
