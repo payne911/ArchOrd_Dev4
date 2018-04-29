e50a15e9d27378971e022f2581cfcb8fcd0fd936
NormTheStorm <incognito@ins-macbook-pro.sf.umontreal.ca>
2018-04-29 15:28:57 -0400

    * more stuff to test

--- asm/swTest.asm
+++ asm/swTest.asm
@@ -1,2 +1,15 @@
-main:	addi $t0 $0 720		# intialiser $t0 a 720
-	sw $t0, 0x10010010	# sauver a l'adresse 0x0010
+main:		addi $v0,$0,360		# intialiser $t0 a 720
+		sll $v0,$v0,1
+#		add $t0,$v0,$0
+#		andi $t0,720
+#		ori $t0,720
+#		addi $t1,$0,32
+#		slt $t2,$t1,$t0
+#		sll $t1,$t1,2
+#		sltu $t2,$t1,$t0
+#		jal jal_l
+#		addi $t2,$t2,4
+#		j the_end
+#jal_l:		addi $t2,$t2,4
+#		jr $ra
+the_end:	sw $v0,0x10010010	# sauver a l'adresse 0x0010
